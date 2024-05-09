06173e933daf80f54770cf9dd837fdd72e21167d
Trafenone <zelinskijvlad5@gmail.com>
2024-05-09 13:03:00 +0300

    Added Swagger generator.

--- Api/Program.cs
+++ Api/Program.cs
@@ -1,8 +1,20 @@
+using Microsoft.OpenApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Version = "v1",
+        Title = "ChicLink API",
+        Description = "An ASP.NET Core Web API for ChicLink"
+    });
+});
 
 var app = builder.Build();
 
@@ -13,5 +25,15 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 
 app.MapControllers();
+
+if(app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI(options =>
+    {
+        options.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
+        options.RoutePrefix = string.Empty;
+    });
+}
 
 app.Run();
