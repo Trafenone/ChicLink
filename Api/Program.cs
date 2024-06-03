9dabd5ce6962972f20d6cf2bc5c5344351f19907
Trafenone <zelinskijvlad5@gmail.com>
2024-06-03 20:08:19 +0300

    Added custom swagger-ui styles.

--- Api/Program.cs
+++ Api/Program.cs
@@ -83,6 +83,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI(options =>
     {
         options.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
+        options.InjectStylesheet("/swagger-ui/custom.css");
         options.RoutePrefix = string.Empty;
     });
 }
